960d4548bd0fbe59b33f7b2761b6e6c0d529e518
amrrs <1littlecoder@gmail.com>
2021-02-25 16:08:35 +0530

    Create r-class-1.R

---
+++ r-class-1.R
@@ -0,0 +1,12 @@
+month.abb[2:4]
+[1] "Feb" "Mar" "Apr"
+> days <- c("Mon","Tue","Wed")
+> 
+> week_end <- c("Sat","Sun")
+> 
+> more_days <- c(days,"Thu","Fri",week_end)
+> 
+> more_days
+[1] "Mon" "Tue" "Wed" "Thu" "Fri"
+[6] "Sat" "Sun"
+> 
